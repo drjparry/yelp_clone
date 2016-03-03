0549a95d0c90456f88a0b6238c926f7095aeebb2
Alain Lemaire <jaxdid@gmail.com>
2016-03-03 20:22:47 +0100

    Implemented OmniAuth to Validate A User via Facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.omniauth :facebook, "450943251775741", "2bdcb9f5ac9a96be03974614a7299aab"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
