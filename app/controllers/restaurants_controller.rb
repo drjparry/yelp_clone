2bad32e12348f755c7c2575df71dd1546aeee0a7
Alain Lemaire <jaxdid@gmail.com>
2016-03-03 12:46:26 +0100

    Added create restaurant functionality
    - Added /restaurants/new view with webform.
    - Updated restaurants controller logic with create action
    - Incorporated permit into params access

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,18 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    puts "Form submission -> Name: #{params[:name]}"
+    puts "Form submission -> Restaurant: #{params[:restaurant]}"
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
 end
