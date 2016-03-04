1cf645c7e9c81e2ae1822395773f0d24969a7b30
David Parry <drjparry@gmail.com>
2016-03-04 19:32:30 +0100

    added jquery to dynamically change endorsement count

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
+
 feature 'endorsing reviews' do
   before do
     kfc = Restaurant.create(name: 'KFC')
     kfc.reviews.create(rating: 3, thoughts: "It was an abomination")
   end
 
-  scenario 'a user can endores a review, which updates the review endorsment count' do
+  scenario 'a user can endores a review, which updates the review endorsment count', js: true do
+    visit '/restaurants'
     click_link 'Endorse Review'
     expect(page).to have_content('1 endorsement')
   end
