b983594af7b2cbe389623e7d4df50d1cb54eccfe
Alain Lemaire <jaxdid@gmail.com>
2016-03-04 18:53:04 +0100

    Updated endorsements.js logic to get and overwrite number of endorsements as appropriate

--- app/assets/javascripts/endorsements.js
+++ app/assets/javascripts/endorsements.js
@@ -1,4 +1,12 @@
-$(document).ready(function()) {
+$(document).ready(function() {
 
-  $('.endorsements')
-}
+  $('.endorsements-link').on('click', function(event) {
+    event.preventDefault();
+
+    var endorsementCount = $( this ).siblings( '.endorsements_count' );
+
+    $.post( this.href, function(response) {
+      endorsementCount.text( response.new_endorsement_count );
+    });
+  });
+});
